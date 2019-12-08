0a3fe53773dfd8261a44961f5721a6ae32336148
hammad001 <hammadabdullahayyubi@gmail.com>
2019-12-08 08:05:00 -0800

    Validation set evaluation

--- train_vcr.sh
+++ train_vcr.sh
@@ -11,6 +11,11 @@ then
     echo 'In Debug mode'
     python -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 train_tasks.py --bert_model bert-base-uncased --from_pretrained save/pytorch_model_9.bin  --from_teacher save/pytorch_model_19.bin --config_file config/bert_base_6layer_6conect.json  --learning_rate 2e-5 --num_workers 10 --tasks 1 --save_name vilbert-loss-debug --debug
 
+elif [ $1 = 'eval' ];
+then
+    echo ' Eval mode'
+    python -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 eval_tasks_subset.py --bert_model bert-base-uncased --from_pretrained save/pytorch_model_9.bin  --from_teacher save/pytorch_model_19.bin --config_file config/bert_base_6layer_6conect.json  --learning_rate 2e-5 --num_workers 10 --tasks 1 --save_name vilbert-loss-debug --debug
+
 else
     python -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --nnodes=1 --node_rank=0 train_tasks.py --bert_model bert-base-uncased --from_pretrained save/pytorch_model_9.bin --from_teacher save/pytorch_model_19.bin --config_file config/bert_base_6layer_6conect.json  --learning_rate 2e-5 --num_workers $num_workers --tasks 1 --save_name $save_name --batch_size $bs --data_root $droot
 
